275ab8385121177c144f7c912918b3aca6dd2239
BabyJIAN <2290653824@qq.com>
2023-06-10 08:48:05 +0800

    feat:base serializer

---
+++ rpc-common/src/main/java/com/zj/rpccommon/exception/SerializeException.java
@@ -0,0 +1,12 @@
+package com.zj.rpccommon.exception;
+
+/**
+ * @author zhengjian
+ * @date 2023-06-09 19:02
+ */
+public class SerializeException extends RuntimeException {
+
+    public SerializeException(String message){
+        super(message);
+    }
+}
---
+++ rpc-server/src/main/java/com/zj/rpcserver/serializer/JDKSerializer.java
@@ -0,0 +1,36 @@
+package com.zj.rpcserver.serializer;
+
+import com.zj.rpccommon.exception.SerializeException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author zhengjian
+ * @date 2023-06-09 18:55
+ */
+public class JDKSerializer implements Serializer{
+    @Override
+    public byte[] serialize(Object obj) {
+        try(ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(byteArrayOutputStream)){
+            oos.writeObject(obj);
+            oos.flush();
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception e) {
+            throw new SerializeException("JDKSerializer failed:"+e.getMessage());
+        }
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(byteArrayInputStream)) {
+            Object o = ois.readObject();
+            return clazz.cast(o);
+        } catch (Exception e){
+            throw new SerializeException("JDKDeSerialize failed:"+e.getMessage());
+        }
+    }
+}
